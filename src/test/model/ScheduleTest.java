bb89c9da0f06eada20a2fd59d5ae7bad967df154
yajasm <yajasm@students.cs.ubc.ca>
2020-10-13 14:48:22 -0700

    Added test classes for classes in Model. Finished ScheduleTest.java

--- src/test/model/ScheduleTest.java
+++ src/test/model/ScheduleTest.java
@@ -3,6 +3,8 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScheduleTest {
@@ -29,41 +31,66 @@ class ScheduleTest {
 
     @Test
     public void testRemoveTask() {
+        schedule.addTask(task1);
+        schedule.removeTask(task1);
 
+        assertEquals(0, schedule.getTasks().size());
     }
 
     @Test
     public void testGetTasks() {
+        schedule.addTask(task1);
+        ArrayList<Task> testList = new ArrayList<>();
+        testList.add(task1);
 
+        assertTrue(testList.equals(schedule.getTasks()));
     }
 
     @Test
     public void testAddLab() {
+        schedule.addLab(lab1);
 
+        assertEquals(1, schedule.getLabs().size());
     }
 
     @Test
     public void testRemoveLab() {
+        schedule.addLab(lab1);
+        schedule.removeLab(lab1);
 
+        assertEquals(0, schedule.getLabs().size());
     }
 
     @Test
     public void testGetLabs() {
+        schedule.addLab(lab1);
+        ArrayList<Lab> testList = new ArrayList<>();
+        testList.add(lab1);
 
+        assertTrue(testList.equals(schedule.getLabs()));
     }
 
     @Test
     public void testAddTutorial() {
+        schedule.addTutorial(tutorial1);
 
+        assertEquals(1, schedule.getTutorials().size());
     }
 
     @Test
     public void testRemoveTutorial() {
+        schedule.addTutorial(tutorial1);
+        schedule.removeTutorial(tutorial1);
 
+        assertEquals(0, schedule.getTutorials().size());
     }
 
     @Test
     public void testGetTutorials() {
+        schedule.addTutorial(tutorial1);
+        ArrayList<Tutorial> testList = new ArrayList<>();
+        testList.add(tutorial1);
 
+        assertTrue(testList.equals(schedule.getTutorials()));
     }
 }
