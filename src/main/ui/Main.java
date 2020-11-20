efadb4bca3a168b6ced5090bc71da124d9fec036
yajasm <yajasm@students.cs.ubc.ca>
2020-11-20 08:21:58 -0800

    GUI: Added ability to create Task, SAVE and LOAD

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,13 +1,8 @@
 package ui;
 
-import java.io.FileNotFoundException;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            new TaskerApp();
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to run application: file not found");
-        }
+        new TaskerGUI();
+
     }
 }
--- src/main/ui/TaskerGUI.java
+++ src/main/ui/TaskerGUI.java
@@ -1,6 +1,7 @@
 package ui;
 
 import model.Schedule;
+import model.Task;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
