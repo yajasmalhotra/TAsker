aed74b3a157f14bc3b74396e5b09db38f3b5ff23
yajasm <yajasm@students.cs.ubc.ca>
2020-11-20 13:23:33 -0800

    Started from scratch. Added ability to create/display tasks + save/load

---
+++ src/main/ui/TaskerGUI2.java
@@ -0,0 +1,134 @@
+package ui;
+
+import model.Schedule;
+import model.Task;
+import persistence.JsonReader;
+import persistence.JsonWriter;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TaskerGUI2 {
+
+    private JPanel panelMain;
+    private JTabbedPane tabbedPane1;
+    private JButton saveButton;
+    private JButton loadButton;
+    private JTextField taskNameTextField;
+    private JButton makeTaskButton;
+    private JButton displayTasksButton;
+    private JTextArea displayTasksTextArea;
+    private ImageIcon image1;
+    private JLabel imageLabel;
+
+    private static final String JSON_STORE = "./data/schedule.json";
+    private static Schedule schedule;
+    private Scanner input;
+    private static JsonWriter jsonWriter;
+    private static JsonReader jsonReader;
+
+
+    public TaskerGUI2() {
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Tasker");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(600, 600);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
+        frame.add(panel);
+
+        jsonReader = new JsonReader(JSON_STORE);
+        jsonWriter = new JsonWriter(JSON_STORE);
+        schedule = new Schedule("Main");
+
+        JButton saveButton = new JButton("Save");
+        saveButton.setBounds(20, 20, 200, 30);
+        panel.add(saveButton);
+        saveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    jsonWriter.open();
+                    jsonWriter.write(schedule);
+                    jsonWriter.close();
+                    System.out.println("Saved " + schedule.getName() + " to " + JSON_STORE);
+                } catch (FileNotFoundException p) {
+                    System.out.println("Unable to write to file: " + JSON_STORE);
+                }
+            }
+        });
+
+        JButton loadButton = new JButton("Load");
+        loadButton.setBounds(300, 20, 200, 30);
+        panel.add(loadButton);
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    schedule = jsonReader.read();
+                    System.out.println("Loaded " + schedule.getName() + " from " + JSON_STORE);
+                } catch (IOException p) {
+                    System.out.println("Unable to read from file: " + JSON_STORE);
+                }
+            }
+        });
+
+        JLabel taskNameLabel = new JLabel("Enter Task Name:");
+        taskNameLabel.setBounds(40, 100, 300, 30);
+        panel.add(taskNameLabel);
+
+        JTextField taskNameTextField = new JTextField();
+        taskNameTextField.setBounds(40, 150, 500, 30);
+        panel.add(taskNameTextField);
+
+        JButton makeTaskButton = new JButton("Create Task");
+        makeTaskButton.setBounds(40, 190, 300, 30);
+        panel.add(makeTaskButton);
+        makeTaskButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Task newTask = new Task(taskNameTextField.getText());
+                schedule.addTask(newTask);
+            }
+        });
+
+        JTextArea displayTasksTextArea = new JTextArea();
+        displayTasksTextArea.setBounds(40, 350, 500, 500);
+        panel.add(displayTasksTextArea);
+
+        JButton displayTasksButton = new JButton("Display Created Tasks");
+        displayTasksButton.setBounds(40, 300, 300, 30);
+        panel.add(displayTasksButton);
+        displayTasksButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String tasks = "";
+
+                for (int i = 0; i < schedule.getTasks().size(); i++) {
+                    tasks += (i + 1) + "." + schedule.getTasks().get(i).getName() + ":\n"
+                            + "Status: " + schedule.getTasks().get(i).getStatus() + "\n";
+                }
+
+                displayTasksTextArea.setText(tasks);
+            }
+        });
+
+
+        frame.setVisible(true);
+    }
+
+
+
+}
