0c19b35c41dc3e74b20982bebfe9c0578cc199d2
yajasm <yajasm@students.cs.ubc.ca>
2020-11-20 14:47:27 -0800

    Run TaskerGUI2.java for GUI

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,8 +1,13 @@
 package ui;
 
+import java.io.FileNotFoundException;
+
 public class Main {
     public static void main(String[] args) {
-        new TaskerGUI();
-
+        try {
+            new TaskerApp();
+        } catch (FileNotFoundException e) {
+            System.out.println("Unable to run application: file not found");
+        }
     }
 }
